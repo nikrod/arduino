73825f7170b84c049c4d431dfe7d992b30752eb0
nikrod <xniko14@hotmail.com>
2016-05-19 03:29:25 -0400

    servos
    se agrega manejo de servos

--- sketch.ino
+++ sketch.ino
@@ -1,11 +1,19 @@
-int ENABLE=5; //MOTOR A
-int IN1=6; //in 1
-int IN2=7; //in 2 
-int ENABLE2=11;//MOTOR B
-int IN3=9;
-int IN4=10;
+#include <Servo.h>
+
+Servo miServo;
+Servo miServo2;
+int angulo=90;
+int angulo2=90;
+
+int ENABLE=4; //MOTOR A
+int IN1=5; //motor A1
+int IN2=6; //motor A2
+int ENABLE2=12;//MOTOR B
+int IN3=10; //motor B1
+int IN4=11; //motor B2
 int mssg = 0; //variable para guardar el mensaje
 int mssg2 = 255; 
+
 void avanzar(int mssg)
 {
           analogWrite(IN1,255-mssg);//inverso
@@ -20,6 +28,9 @@ void setup()
   pinMode(IN2,OUTPUT);
   pinMode(IN3,OUTPUT);
   pinMode(IN4,OUTPUT);
+  miServo.attach(9); //declaración de pin 9 para servo1
+  miServo2.attach(3); //declaración de pin 3 para servo2
+  
   digitalWrite(ENABLE, HIGH);// (pin,high =1 low=0)
   digitalWrite(ENABLE2,HIGH); 
   Serial.setTimeout(50);
@@ -33,12 +44,10 @@ void loop()
      
   
   if (Serial.available() > 0)
-   
   {
-     
-       
         mssg = Serial.parseInt(); 
-        
+        angulo=constrain(angulo,0,180); //restricción de angulo
+        angulo2=constrain(angulo2,0,180);//restricción de angulo2
         if(mssg==8)
         {       
               analogWrite(IN1,255-mssg2); //adelante
@@ -71,45 +80,38 @@ void loop()
               digitalWrite(IN3,HIGH);    
         
         }        
+
+        else if(mssg==1)
+        {    
+              angulo=angulo+10;  
+              miServo.write(angulo); 
+        }
+
+         else if(mssg==3)
+        {
+              angulo=angulo-10;  
+              miServo.write(angulo);
+        }
+
+         else if(mssg==7)
+        {
+              angulo2=angulo2+10;  
+              miServo2.write(angulo2);
+        }
+
+         else if(mssg==9)
+        {
+              angulo2=angulo2-10;  
+              miServo2.write(angulo2);
+        }
+        
         else
         {
               analogWrite(IN1,255-mssg2);//apagado
               digitalWrite(IN2,LOW);
               analogWrite(IN3,255-mssg2);
-              digitalWrite(IN4,LOW);   
-        
+              digitalWrite(IN4,LOW);      
         }
-     
-     /*if(mssg == '4') // Movimiento hacia la izquierda
-     {
-        digitalWrite(IN1,LOW);
-        digitalWrite(IN2,HIGH);
-        digitalWrite(IN3,LOW);
-        digitalWrite(IN4,HIGH);
-     }
-     if(mssg == '6') //Movimiento hacia la derecha
-     {
-        digitalWrite(IN1,HIGH);
-        digitalWrite(IN2,LOW);
-        digitalWrite(IN3,HIGH);
-        digitalWrite(IN4,LOW);
-     }
-     if(mssg == '2') //Movimiento hacia atrás
-     {
-        digitalWrite(IN1,LOW);
-        digitalWrite(IN2,HIGH);
-        digitalWrite(IN3,HIGH);
-        digitalWrite(IN4,LOW);
-     }
-     if(mssg == '5') //Parar
-     {
-        
-        digitalWrite(IN1,LOW);
-        digitalWrite(IN2,LOW);
-        digitalWrite(IN3,LOW);
-        digitalWrite(IN4,LOW);
-     }
-     */
    }
 }
 
